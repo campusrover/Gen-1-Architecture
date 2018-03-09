bc3cd94c76485b0bb10c36859783fed02554a932
Alec Rodgers <alecr1997@gmail.com>
2018-03-09 13:22:18 -0500

    Add implenetation for state node

---
+++ src/state.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import rospy
+from std_msgs.msg import Int32
+
+rospy.init_node('state')
+
+def state_callback(msg):
+	state = msg.data
+	mux_pub.publish(state)
+
+navigation_sub = rospy.Subscriber('/fake_navigation', Int32, state_callback )
+teleop_sub = rospy.Subscriber('/state', Int32, state_callback)
+mux_pub = rospy.Publisher('/mux', Int32, queue_size=1)
+
+rospy.spin()
