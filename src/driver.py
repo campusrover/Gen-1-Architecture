f9b237c980f0474a400389ff97b5405cc37dc892
Alec Rodgers <alecr1997@gmail.com>
2018-03-09 13:24:03 -0500

    Fix bug in driver node to communicate with mux

--- src/driver.py
+++ src/driver.py
@@ -4,11 +4,11 @@ from geometry_msgs.msg import Twist
 
 def mux_callback(msg):
 	mux_twist = msg.data
-	cmd_vel_pub.publisher()
+	cmd_vel_pub.publish(mux_twist)
 
 rospy.init_node('driver')
-mux_sub = rospy.Subscriber('mux', Twist, mux_callback)
-cmd_vel_pub = rospy.Publisher('cmd_vel', Twist, queue_size = 1)
+mux_sub = rospy.Subscriber('/mux', Twist, mux_callback)
+cmd_vel_pub = rospy.Publisher('/cmd_vel', Twist, queue_size = 1)
 
 rospy.spin()
 	
